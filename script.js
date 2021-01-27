99e47b44b738f0f93bc0e23fc2b05ab781d67358
David Tran <davtran303@gmail.com>
2021-01-27 02:21:39 -0700

    add in querySelectors

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const question = document.querySelector("#question");
+const choice = Array.from(document.querySelectorAll(".choice-op"));
+const question = document.querySelector("#question");
+const question = document.querySelector("#question");
 
 
 
